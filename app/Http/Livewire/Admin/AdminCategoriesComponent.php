139426780035fa6a2e82ec733ff319a346e86b41
alvinfebrianto <alvinfebrianto.2702@gmail.com>
2023-10-25 10:05:23 +0700

    Admin Delete Category

--- app/Http/Livewire/Admin/AdminCategoriesComponent.php
+++ app/Http/Livewire/Admin/AdminCategoriesComponent.php
@@ -9,6 +9,14 @@ use Livewire\WithPagination;
 class AdminCategoriesComponent extends Component
 {
     use WithPagination;
+    public $category_id;
+
+    public function deleteCategory()
+    {
+        $category = Category::find($this->category_id);
+        $category->delete();
+        session()->flash('message', 'Category has been deleted successfully.');
+    }
 
     public function render()
     {
