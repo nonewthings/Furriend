6e48b210f03ff021fa094279747e26d6f10c1b19
alvinfebrianto <alvinfebrianto.2702@gmail.com>
2023-11-08 23:22:15 +0700

    Show Popular Categories on Home

--- resources/views/livewire/admin/admin-categories-component.blade.php
+++ resources/views/livewire/admin/admin-categories-component.blade.php
@@ -39,8 +39,10 @@
                                     <thead>
                                         <tr>
                                             <th>#</th>
+                                            <th>Image</th>
                                             <th>Name</th>
                                             <th>Slug</th>
+                                            <th>Popular</th>
                                             <th>Action</th>
                                         </tr>
                                     </thead>
@@ -51,8 +53,10 @@
                                         @foreach($categories as $category)
                                             <tr>
                                                 <td>{{ ++$i }}</td>
+                                                <td><img src="{{ asset('assets/imgs/categories') }}/{{$category->image}}" width="60"></td>
                                                 <td>{{ $category->name }}</td>
                                                 <td><a href="{{ route('product.category', ['slug' => $category->slug]) }}">{{ $category->slug }}</a></td>
+                                                <td>{{ $category->is_popular == 1 ? 'Yes':'No' }}</td>
                                                 <td>
                                                     <a href="{{ route('admin.category.edit', ['category_id' => $category->id]) }}" class="text-info">Edit</a>
                                                     <a href="#" class="text-danger" onclick="deleteConfirmation({{ $category->id }})" style="margin-left: 20px;">Delete</a>
