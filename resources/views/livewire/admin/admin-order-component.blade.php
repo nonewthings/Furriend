eef72c8e41f6d75aed6693f2fa31c3ead973b754
alvinfebrianto <alvinfebrianto.2702@gmail.com>
2023-12-04 14:13:45 +0700

    Create Admin Orders Page

---
+++ resources/views/livewire/admin/admin-order-component.blade.php
@@ -0,0 +1,70 @@
+<div>
+    <style>
+        nav svg{
+            height: 20px;
+        }
+        nav .hidden{
+            display: block;
+        }
+    </style>
+    <main class="main">
+        <div class="page-header breadcrumb-wrap">
+            <div class="container">
+                <div class="breadcrumb">
+                    <a href="/" rel="nofollow">Home</a>
+                    <span></span> All Orders
+                </div>
+            </div>
+        </div>
+        <section class="mt-50 mb-50">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        All Orders
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <table class="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>Order Id</th>
+                                            <th>Total</th>
+                                            <th>First Name</th>
+                                            <th>Last Name</th>
+                                            <th>Phone</th>
+                                            <th>Email</th>
+                                            <th>ZIP Code</th>
+                                            <th>Status</th>
+                                            <th>Order Date</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach($orders as $order)
+                                        <tr>
+                                            <td>{{$order->id}}</td>
+                                            <td>Rp{{ number_format($order->total, 0) }}</td>
+                                            <td>{{$order->firstname}}</td>
+                                            <td>{{$order->lastname}}</td>
+                                            <td>{{$order->phone}}</td>
+                                            <td>{{$order->email}}</td>
+                                            <td>{{$order->zipcode}}</td>
+                                            <td>{{$order->status}}</td>
+                                            <td>{{$order->created_at}}</td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                                {{$orders->links()}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</div>
